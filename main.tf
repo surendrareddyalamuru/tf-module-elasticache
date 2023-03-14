3e0571dac70d7b35d882a304008598fc4b99a0bb
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-14 12:32:16 +0530

    rds

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "aws_elasticache_cluster" "main" {
+  cluster_id           = "${var.env}-${var.name}-elasticache"
+  engine               = var.engine
+  engine_version       = var.engine_version
+  node_type            = var.node_type
+  num_cache_nodes      = var.num_cache_nodes
+  parameter_group_name = aws_elasticache_parameter_group.default.name
+  port                 = 6379
+#  security_group_ids   = [aws_security_group.sg.id]
+#  subnet_group_name    = aws_elasticache_subnet_group.default.name
+}
+
+resource "aws_elasticache_parameter_group" "default" {
+  name   = "${var.env}-${var.name}-elasticache"
+  family = "redis6.x"
+}
+
+resource "aws_elasticache_subnet_group" "default" {
+  name       = "${var.env}-${var.name}-elasticache"
+  subnet_ids = var.subnets
+}
---
+++ vars.tf
@@ -0,0 +1,7 @@
+variable "env" {}
+variable "name" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
+variable "subnets" {}
