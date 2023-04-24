1ee2e9c2eed38e1708fbf09d6cb7aa30b4748430
surendra reddy <surendrareddylamuru01@gmail.com>
2023-04-24 10:59:15 +0530

    elasticache

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_elasticache_cluster" "main" {
   num_cache_nodes      = var.num_cache_nodes
   parameter_group_name = aws_elasticache_parameter_group.default.name
   port                 = 6379
-#  security_group_ids   = [aws_security_group.sg.id]
-#  subnet_group_name    = aws_elasticache_subnet_group.default.name
+  security_group_ids   = [aws_security_group.sg.id]
+  subnet_group_name    = aws_elasticache_subnet_group.default.name
 }
 
 resource "aws_elasticache_parameter_group" "default" {
--- vars.tf
+++ vars.tf
@@ -5,3 +5,5 @@ variable "engine_version" {}
 variable "node_type" {}
 variable "num_cache_nodes" {}
 variable "subnets" {}
+variable "vpc_cidr" {}
+variable "vpc_id" {}
